16d9d30c4a41ecdbeb09b6a99a8621a6b0a49d1c
maks909 <114800924+maks909@users.noreply.github.com>
2024-04-02 17:37:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -88,5 +88,5 @@ plus_button.addEventListener("click", function(){
     price_text.innerHTML = (quantity[key]*price).toString() + "zł";
 });
 Telegram.Webapp.onEvent("mainButtonClicked", function(){
-    tg.sendData(quantity);
+    tg.sendData(1);//quantity);
 });
